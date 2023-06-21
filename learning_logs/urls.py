7221538e3b84fe5d9d141716193aa9abb9600bbf
Elman <134933773+Shternoa@users.noreply.github.com>
2023-06-21 09:40:39 +0300

    Редактирование данных и добовление новых записей на темы

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -9,5 +9,7 @@ app_name = 'learning_logs'
 urlpatterns = [
     # Домашняя страница
     path('', views.index, name='index'), path('topics/', views.topics, name='topics'),
-    path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic')
+    path(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+    path(r'^new_topic/$', views.new_topic, name='new_topic'),
+    path(r'^new_entry/(?P<topic_id>\d+)/$', views.new_entry, name='new_entry')
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from .models import Topic
+from .forms import TopicForm, EntryForm
+
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 
 # Create your views here.
@@ -21,3 +25,37 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    """Определение новой темы"""
+    if request.method != 'POST':
+        # Данные не отправились и создается пустая форма
+        form = TopicForm()
+    else:
+        # Отправлены данные POST, обработка данных
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+
+def new_entry(request, topic_id):
+    """Добовляет новую запись по конкретной теме"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != 'POST':
+        # Данные не отправились и созадается новая форма
+        form = EntryForm()
+    else:
+        # Данные отправлены в POST и обрабатываются
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
