e03446d09a46755896fc4c825964b15d4ea04468
Elman <134933773+Shternoa@users.noreply.github.com>
2023-06-22 18:28:38 +0300

    Добавлена регестрация

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from django.contrib.auth import logout
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
@@ -10,3 +11,21 @@ def logout_view(request):
     """Завершение сеанса"""
     logout(request)
     return HttpResponseRedirect(reverse('learning_logs:index'))
+
+
+def register(request):
+    """Регестрация пользователя"""
+    if request.method != 'POST':
+        # Показывает страницу регестрации
+        form = UserCreationForm()
+    else:
+        # Обработка заполенной формы
+        form = UserCreationForm(data=request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            # После входа идем на домашнюю страницу
+            authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
+            login(request, authenticated_user)
+            return HttpResponseRedirect(reverse('learning_logs:index'))
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
