506bd4bcede599f0e76ef86c5cc87157f3f46782
Elman <134933773+Shternoa@users.noreply.github.com>
2023-06-22 18:13:06 +0300

    Добавлен выход

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 """Определяет схемы URL для пользователей"""
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 
 from . import views
 
@@ -8,5 +8,6 @@ app_name = 'users'
 
 urlpatterns = [
     # Страница для входа
-    path('login/', LoginView.as_view(template_name='users/login.html'), name='login')
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', views.logout_view, name='logout')
 ]
